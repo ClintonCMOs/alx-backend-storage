5a05a62104ad1ba7b3d759301716c7fc26dadf18
ClintonCMOs <orengecmo@gmail.com>
2024-08-13 13:15:11 +0300

    a stored procedure ComputeAverageScoreForUser

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,9 +1,17 @@
--- Show and compute average score
-SELECT * FROM users;
-SELECT * FROM corrections;
+-- Create stored procedure ComputeAverageScoreForUser
+-- computes and stores average score for a student
+-- Takes 1 input user_id, a users.id
 
-SELECT "--";
-CALL ComputeAverageScoreForUser((SELECT id FROM users WHERE name = "Jeanne"));
+DELIMITER $$
 
-SELECT "--";
-SELECT * FROM users;
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
+BEGIN
+	UPDATE users
+	SET
+	average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id)
+	WHERE id = user_id;
+
+END $$
+
+DELIMITER ;
